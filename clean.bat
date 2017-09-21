d74214f43deef80513e9bdbb53f14fe59c6b5ed1
BOM-DESKTOP\spring <springnode@gmail.com>
2017-09-22 00:09:59 +0900

    test

--- clean.bat
+++ clean.bat
@@ -4,3 +4,18 @@ IF EXIST "x64" (
 IF EXIST "x86" (
 	RMDIR /S /Q "x86"
 )
+IF EXIST "prj_ISpring\Debug" (
+	RMDIR /S /Q "prj_ISpring\Debug"
+)
+IF EXIST "prj_ISpring\Release" (
+	RMDIR /S /Q "prj_ISpring\Release"
+)
+IF EXIST "prj_ISpring\x64" (
+	RMDIR /S /Q "prj_ISpring\x64"
+)
+IF EXIST "install_ispring\Debug" (
+	RMDIR /S /Q "install_ispring\Debug"
+)
+IF EXIST "install_ispring\Release" (
+	RMDIR /S /Q "install_ispring\Release"
+)
--- install_ispring/install.cpp
+++ install_ispring/install.cpp
@@ -1,8 +1,8 @@
-#ifdef _WIN64
+﻿#ifdef _WIN64
 #error support only x86
 #endif
 #ifdef _DEBUG
-#error support only release
+//#error support only release
 #endif
 /*
 *  install.cpp
@@ -24,18 +24,61 @@
 #include"../prj_ISpring/ispring/Compression.h"
 #include"../prj_ISpring/ispring/File.h"
 
+#include<WinInet.h>
+#pragma comment(lib, "wininet.lib")
+
+std::string url_gist = "https://gist.github.com/springkim/8429bb12102fc14cdd58cc11ee7bd273";
+
+
+std::string GetHtml(std::string url) {
+	std::string html;
+	HINTERNET hInternet = InternetOpen(TEXT("HTTP"), INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);	//인터넷 관련 DLL을 초기화한다.
+	if (hInternet) {
+		HINTERNET hUrl = InternetOpenUrlA(hInternet, url.c_str(), NULL, 0, INTERNET_FLAG_RELOAD, 0);	//url에 걸린 파일을 연다.
+		if (hUrl) {
+			DWORD realSize = 0;
+			DWORD possibleSize = 0;
+			DWORD recv = 0;
+			char* buffer = new char[2000000];
+			char* tempBuffer = new char[2000000];
+			memset(buffer, 0, 2000000 * sizeof(char));
+			memset(tempBuffer, 0, 2000000 * sizeof(char));
+			do {
+				InternetQueryDataAvailable(hUrl, &possibleSize, 0, 0);
+				InternetReadFile(hUrl, buffer, possibleSize, &realSize);
+				if (possibleSize>0) {
+					memcpy(tempBuffer + recv, buffer, possibleSize * sizeof(char));
+				}
+				recv += possibleSize;
+			} while (realSize != 0);
+			html.resize(realSize);
+			html = tempBuffer;
+			delete[] buffer;
+			delete[] tempBuffer;
+		}
+	}
+	return html;
+}
 void Fail(std::string str) {
 	std::cerr << str << std::endl;
 	system("pause");
 	exit(EXIT_FAILURE);
 }
+
 int main() {
-	char _temp[MAX_PATH];
+	std::string html=GetHtml(url_gist);
+	std::string tag_begin = "&lt;ispring-version&gt;";
+	std::string tag_end= "&lt;/ispring-version&gt;";
+	size_t pos_beg = html.find(tag_begin) + tag_begin.length();
+	size_t pos_end = html.find(tag_end);
+	std::string version = html.substr(pos_beg, pos_end-pos_beg);
+	std::cout << version << std::endl;
+	/*char _temp[MAX_PATH];
 	GetTempPathA(MAX_PATH - 1, _temp);
 	std::string temp = _temp;
 
-	std::string file = temp + "MSpring.zip";
-	std::cout << "Downloading MSpring lastest version..." << std::endl;
+	std::string file = temp + "ISpring.zip";
+	std::cout << "Downloading ISpring lastest version..." << std::endl;
 	if (PathFileExistsA(file.c_str()) == TRUE) {
 		std::cout << "Remove current version..." << std::endl;
 		DeleteFileA("MSpring.zip");
@@ -51,6 +94,6 @@ int main() {
 	if (CopyFolder(src, ".\\") == FALSE) {
 		Fail("Move file fail");
 	}
-	std::cout << "Successfully updated" << std::endl;
+	std::cout << "Successfully updated" << std::endl;*/
 	return 0;
 }
