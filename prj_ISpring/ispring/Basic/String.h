453045f0ac4ba66b7f7b5229e12e3420e7a2db26
BOM-DESKTOP\spring <springnode@gmail.com>
2017-10-04 18:50:50 +0900

    ispring

--- prj_ISpring/ispring/Basic/String.h
+++ prj_ISpring/ispring/Basic/String.h
@@ -25,7 +25,8 @@ namespace ispring {
 		*	@param std::string token : token
 		*	@return Divided words
 		*	@warning Remove 0-length string automatically
-		*	@details ( ";*.cpp;*.h;*.c;*.jpg;;;;;") -> ([*.cpp],[*.h],[*.c],[*.jpg])
+		*	@details input -> ";*.cpp;*.h;*.c;*.jpg;;;;;" 
+				      output -> {"*.cpp","*.h","*.c","*.jpg"}
 		*/
 		static std::vector<std::string> Tokenizer(std::string str, std::string token) {
 			std::vector<std::string> ret;
